cb3ea1dab0429337572e470dc940dc41395f0be2
svetlana <pivovarova.svetlana@gmail.com>
2023-03-09 17:23:53 +0400

    [ FEAT ] add Main Section styles

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,7 +1,24 @@
-function Content() {
+import architecture from "./../../assets/Architecture.png";
+import { TEXTS } from "../../utils/constants";
+import "./Content.css";
+
+function Content({
+  heading = TEXTS[0].heading,
+  description = TEXTS[0].text,
+  img = architecture,
+}) {
   return (
-    <section>
-      <div></div>
+    <section className="content">
+      <div className="content-container">
+        <article className="content-info">
+          <h2 className="content-heading">{heading}</h2>
+          <p className="content-text">{description}</p>
+          <p className="content-pagination">
+            {1}/{6}
+          </p>
+        </article>
+        <img className="content-illustration" src={img} alt={heading} />
+      </div>
     </section>
   );
 }
